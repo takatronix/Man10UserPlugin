d2bca9cf3258b51f49526faa10bec0057c6870f3
Takashi Ohtsuka <takatronix@gmail.com>
2019-04-04 16:04:07 +0900

    1st commit

--- src/main/kotlin/Man10UserPlugin.kt
+++ src/main/kotlin/Man10UserPlugin.kt
@@ -65,11 +65,13 @@ class Man10UserPlugin : JavaPlugin() , Listener {
         try{
             var rs = mysql.query("select * from pda_player_data where uuid = '${p.uniqueId}'");
 
+            rs?.next()
             if(rs == null){
                 mysql.close()
                 showError(p,"データベース接続エラー")
                 return -1
             }
+
 
             var name = rs.getString("name")
 
@@ -78,8 +80,37 @@ class Man10UserPlugin : JavaPlugin() , Listener {
                 return -1
             }
 
-            broadcast("${p.name}の過去の名前は${name}")
+            if(name == p.name){
+                return 0
+            }
 
+
+            Thread.sleep(3500)
+
+            broadcast("§c${name}が名前を${p.name}に変更したことを検出！ 過去の犯罪履歴/スコアを照合しています....")
+            Thread.sleep(2500)
+            var ret = mysql.execute("update pda_player_data set name = '${p.name}' where uuid = '${p.uniqueId}'")
+            if(!ret){
+                broadcast("§c${name}が名前を${p.name}に変更した呪いによりスコアを取得できなくなった...")
+                return -1
+            }
+            broadcast("§e${name}が名前を${p.name}に変更したことを通知するため国王を呼び出しています..... §kxxxx")
+
+            Bukkit.broadcastMessage("§8[国王] あっ？ぁ・・？？呼んだ？？？。いま、、ねてる・・・ぐぅ・・")
+            Thread.sleep(2500)
+            broadcast("§e${name}が名前を${p.name}に変更したことを通知するため国王を呼び出しています..... §kxxxx")
+            Thread.sleep(2000)
+            Bukkit.broadcastMessage("§8[国王] なんだって！！！！（驚）${name}が名前を${p.name}に変更しただと！！")
+            Thread.sleep(2000)
+            Bukkit.broadcastMessage("§8[国王] 名前変える頻繁に変えるやつだいたい犯罪者だからな、、えっと、、")
+            Thread.sleep(2000)
+            Bukkit.broadcastMessage("§8[国王] §e${name}(${p.name})の過去の犯罪履歴を登録・・。タァーン！（リターンキーを叩く）")
+            Thread.sleep(2000)
+            Bukkit.broadcastMessage("§8[国王] 過去データ更新処理、めんどくせえーー。")
+            Thread.sleep(2000)
+            Bukkit.broadcastMessage("§e[国王] ${name}(${p.name})の名前変更処理が完了！")
+            Thread.sleep(2000)
+            Bukkit.broadcastMessage("§8[国王] ${name}(${p.name})をブラックリストに登録っ！！。タァーン！（リターンキーを叩く）")
 
         }
         catch (e:Exception)
